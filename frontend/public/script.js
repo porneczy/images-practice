0c55ee9f85ea7a5bb718ff6f8b1e964a6d886637
porneczy <porneczy@gmail.com>
2022-04-06 16:34:43 +0200

    v1

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,4 +1,37 @@
+const parseJSON = async (url) => {
+    const response = await fetch(url)
+    return response.json()
+}
+
+const swiperComponent = (data, component) => {
+    return `
+        <div class="swiper">
+            <div class="swiper-wrapper"> 
+                ${data.map(img => component(img)).join("")}
+            </div>
+        </div>
+    `
+}
+
+const swiperSlideComponent = ({fileName, title}) => {
+    return `
+        <div class="swiper-slide">
+            <h2>${title}</h2>
+            <img src="/pub/img/${fileName}">
+        </div>
+    `
+}
+
 const loadEvent = async () => {
+    const rootElement = document.getElementById("root")
+    const result = await parseJSON("/image-list")
+
+    rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent))
+    //mivel callback function nem kell meghivni, lefuttatni csak beadni neki
     
+    const swiper = new Swiper(".swiper", {
+        loop: true,
+    })
+    console.log(swiper);
 }
 window.addEventListener("load", loadEvent);
